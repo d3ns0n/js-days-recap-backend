32fe17c14db04f4ca3f866d61d2c5b9888070fe3
Dennis Lotz <dennis.lotz@idealo.de>
2017-10-19 16:30:50 +0200

    enable CORS for TaskRepository

--- src/main/java/tasks/repositories/TaskRepository.java
+++ src/main/java/tasks/repositories/TaskRepository.java
@@ -1,7 +1,9 @@
 package tasks.repositories;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import tasks.model.Task;
 
+@CrossOrigin
 public interface TaskRepository extends CrudRepository<Task, Long> {}
