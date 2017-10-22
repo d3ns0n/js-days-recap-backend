9dfb7838161f5f156a5c62aa0eb05488fc503e46
Dennis Lotz <dennis.lotz@idealo.de>
2017-10-22 13:32:12 +0200

    expose task ids

---
+++ src/main/java/tasks/configuration/ExposeEntityIdRestConfiguration.java
@@ -0,0 +1,16 @@
+package tasks.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
+
+import tasks.model.Task;
+
+@Configuration
+public class ExposeEntityIdRestConfiguration extends RepositoryRestConfigurerAdapter {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        config.exposeIdsFor(Task.class);
+    }
+}
