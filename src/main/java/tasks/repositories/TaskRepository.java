93ebf318a61d8d2076cedb8bb2daf8fb0d940b95
Dennis Lotz <dennis.lotz@idealo.de>
2017-10-24 13:51:23 +0200

    add findByTaskListId

--- src/main/java/tasks/repositories/TaskRepository.java
+++ src/main/java/tasks/repositories/TaskRepository.java
@@ -1,9 +1,14 @@
 package tasks.repositories;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import tasks.model.Task;
 
 @CrossOrigin
-public interface TaskRepository extends CrudRepository<Task, Long> {}
+public interface TaskRepository extends CrudRepository<Task, Long> {
+    List<Task> findByTaskListId(@Param("taskListId") Long taskListId);
+}
